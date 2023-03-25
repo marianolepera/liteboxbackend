d05f72563bae5cffb868e42ea6abe6c8d454b755
marianolepera <marianolepera@hotmail.com>
2023-03-25 05:19:30 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(morgan(("dev")))
 app.use(cors())
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '50mb', extended: true}));
 app.use(express.urlencoded({ limit: '50mb', extended: true}));
 
 app.get("/", (req, res) => {
