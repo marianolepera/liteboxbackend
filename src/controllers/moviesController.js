36dc45caa3e18c2f4cd253b235529dafa1badea4
marianolepera <marianolepera@hotmail.com>
2023-03-25 09:15:38 -0300

    nvbnvnvb

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -14,7 +14,6 @@ const configuracionMulter = {
         },
         
     }),
-    limits: { fieldSize: 25 * 1024 * 1024 },
     fileFilter(req, file, cb) {
         if ( file.mimetype === 'image/jpeg' ||  file.mimetype ==='image/png' ) {
             cb(null, true);
