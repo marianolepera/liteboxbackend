7f9294132f28a0807414269c43c06908cb03dc70
marianolepera <marianolepera@hotmail.com>
2023-03-25 05:26:54 -0300

    gfhgf

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(morgan(("dev")))
 app.use(cors())
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({limit : 52428800}));
 app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
