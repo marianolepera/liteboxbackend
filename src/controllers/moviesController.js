d3f20c683b6d52878093d2c9b64a12891180cdd3
marianolepera <marianolepera@hotmail.com>
2023-03-25 09:05:52 -0300

    filesize agregado

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -11,8 +11,12 @@ const configuracionMulter = {
         filename: (req, file, cb) => {
             const extension = file.mimetype.split('/')[1];
             cb(null, `${shortid.generate()}.${extension}`);
-        }
+        },
+        
     }),
+    limits: {
+        fileSize: 52428800
+      },
     fileFilter(req, file, cb) {
         if ( file.mimetype === 'image/jpeg' ||  file.mimetype ==='image/png' ) {
             cb(null, true);
