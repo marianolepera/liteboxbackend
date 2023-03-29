5440f3add3c695d66efea375500e8a1f8f08ad80
marianolepera <marianolepera@hotmail.com>
2023-03-29 03:54:48 -0300

    desactivado multer

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,40 +1,40 @@
 import Movie from "../models/movies"
 import axios from 'axios';
-import multer from "multer"
-import shortid from "shortid";
+// import multer from "multer"
+// import shortid from "shortid";
 
-const configuracionMulter = {
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, __dirname+'../uploads/');
-        },
-        filename: (req, file, cb) => {
-            const extension = file.mimetype.split('/')[1];
-            cb(null, `${shortid.generate()}.${extension}`);
-        },
+// const configuracionMulter = {
+//     storage: multer.diskStorage({
+//         destination: (req, file, cb) => {
+//             cb(null, __dirname+'../uploads/');
+//         },
+//         filename: (req, file, cb) => {
+//             const extension = file.mimetype.split('/')[1];
+//             cb(null, `${shortid.generate()}.${extension}`);
+//         },
         
-    }),
-    fileFilter(req, file, cb) {
-        if ( file.mimetype === 'image/jpeg' ||  file.mimetype ==='image/png' ) {
-            cb(null, true);
-        } else {
-            cb(new Error('Formato No válido'))
-        }
-    },
-}
+//     }),
+//     fileFilter(req, file, cb) {
+//         if ( file.mimetype === 'image/jpeg' ||  file.mimetype ==='image/png' ) {
+//             cb(null, true);
+//         } else {
+//             cb(new Error('Formato No válido'))
+//         }
+//     },
+// }
 
-// pasar la configuración y el campo
-const upload = multer(configuracionMulter).single('backdrop_path');
+// // pasar la configuración y el campo
+// const upload = multer(configuracionMulter).single('backdrop_path');
 
-// Sube un archivo 
-export const subirArchivo = (req, res, next) => {
-    upload(req, res, function(error) {
-        if(error) {
-            res.json({mensaje: error})
-        }
-        return next();
-    })
-}
+// // Sube un archivo 
+// export const subirArchivo = (req, res, next) => {
+//     upload(req, res, function(error) {
+//         if(error) {
+//             res.json({mensaje: error})
+//         }
+//         return next();
+//     })
+// }
 
 export const createMovie = async (req,res,next)=>{
     // const newMovie = new Movie({
@@ -44,9 +44,9 @@ export const createMovie = async (req,res,next)=>{
     const newMovie = new Movie(req.body);
 
     try {
-        if(req.file.filename) {
-            newMovie.backdrop_path = req.file.filename
-        }
+        // if(req.file.filename) {
+        //     newMovie.backdrop_path = req.file.filename
+        // }
         const movieSaved = await newMovie.save();
         res.status(201).json(movieSaved);
     } catch (error) {
