60b24c3266a47301d656201f621c2976999cc1b4
marianolepera <marianolepera@hotmail.com>
2023-03-29 04:18:04 -0300

    adas

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(morgan(("dev")))
 app.use(cors())
 app.use(helmet());
-app.use(express.json({limit : 52428800}));
-app.use(express.urlencoded({ extended: true,limit : 52428800}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true,}));
 
 app.get("/", (req, res) => {
     res.json({
