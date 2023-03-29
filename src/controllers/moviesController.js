2c54d4fa8ebe19c742a114efca2dec1ae3509181
marianolepera <marianolepera@hotmail.com>
2023-03-29 04:54:50 -0300

    corregido

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,52 +1,10 @@
 import Movie from "../models/movies"
 import axios from 'axios';
-// import multer from "multer"
-// import shortid from "shortid";
-
-// const configuracionMulter = {
-//     storage: multer.diskStorage({
-//         destination: (req, file, cb) => {
-//             cb(null, __dirname+'../uploads/');
-//         },
-//         filename: (req, file, cb) => {
-//             const extension = file.mimetype.split('/')[1];
-//             cb(null, `${shortid.generate()}.${extension}`);
-//         },
-        
-//     }),
-//     fileFilter(req, file, cb) {
-//         if ( file.mimetype === 'image/jpeg' ||  file.mimetype ==='image/png' ) {
-//             cb(null, true);
-//         } else {
-//             cb(new Error('Formato No válido'))
-//         }
-//     },
-// }
-
-// // pasar la configuración y el campo
-// const upload = multer(configuracionMulter).single('backdrop_path');
-
-// // Sube un archivo 
-// export const subirArchivo = (req, res, next) => {
-//     upload(req, res, function(error) {
-//         if(error) {
-//             res.json({mensaje: error})
-//         }
-//         return next();
-//     })
-// }
 
 export const createMovie = async (req,res,next)=>{
-    // const newMovie = new Movie({
-    //     original_title: req.body.original_title,
-    //     backdrop_path: req.body.backdrop_path,
-    // })
     const newMovie = new Movie(req.body);
 
     try {
-        // if(req.file.filename) {
-        //     newMovie.backdrop_path = req.file.filename
-        // }
         const movieSaved = await newMovie.save();
         res.status(201).json(movieSaved);
     } catch (error) {
@@ -62,7 +20,7 @@ export const getMovies = async (req,res,next)=>{
             `https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20`
         );
         
-        const finalMovies=movies.concat(moviesDB.data.results)
+        const finalMovies=moviesDB.data.results.concat(movies)
         res.json(finalMovies);
     } catch (error) {
         console.log(error);
